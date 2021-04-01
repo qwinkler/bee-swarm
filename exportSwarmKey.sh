353ef1cd1fcca1b7652fd28dfcb27f91ace4fb52
Artem Matyhin <artem.matyhin-ae@transvoyant.com>
2021-04-01 12:19:27 +0300

    fixed indent

--- exportSwarmKey.sh
+++ exportSwarmKey.sh
@@ -7,32 +7,32 @@ commitID="main"
 exportedKeysPath="/root/bee-keys"
 
 if [ $(id -u) != "0" ]; then
-    echo "You have to run this scipt as a root user. Aborting."
-    exit 1
+  echo "You have to run this scipt as a root user. Aborting."
+  exit 1
 fi
 
 install() {
-	if [ -f key.json ]; then
-		rm key.json
-	fi
+  if [ -f key.json ]; then
+  rm key.json
+  fi
 
-	wget -qO exportSwarmKey https://raw.githubusercontent.com/qwinkler/bee-swarm/${commitID}/exportSwarmKey
-	chmod +x exportSwarmKey
+  wget -qO exportSwarmKey https://raw.githubusercontent.com/qwinkler/bee-swarm/${commitID}/exportSwarmKey
+  chmod +x exportSwarmKey
 
-	echo "Enter the node password:"
-	read  n
-	echo 'Creating the private key'
+  echo "Enter the node password:"
+  read  n
+  echo 'Creating the private key'
 
-	mkdir -p $exportedKeysPath
-	cp ${BASEDIR}/data/keys/swarm.key ${exportedKeysPath}/swarm.key
-	./exportSwarmKey ${exportedKeysPath} $n > key_tmp.json
-	rm ${exportedKeysPath}/swarm.key
-	sed 's/^[^{]*//' key_tmp.json > key.json
-	rm key_tmp.json
+  mkdir -p $exportedKeysPath
+  cp ${BASEDIR}/data/keys/swarm.key ${exportedKeysPath}/swarm.key
+  ./exportSwarmKey ${exportedKeysPath} $n > key_tmp.json
+  rm ${exportedKeysPath}/swarm.key
+  sed 's/^[^{]*//' key_tmp.json > key.json
+  rm key_tmp.json
   rmdir $exportedKeysPath
-	echo "Your wallet: $(cat key.json | jq '.address')"
-	echo "Your private key for export: $(cat key.json | jq '.privatekey')"
-	echo "Private key file path: $(pwd)/key.json"
+  echo "Your wallet: $(cat key.json | jq '.address')"
+  echo "Your private key for export: $(cat key.json | jq '.privatekey')"
+  echo "Private key file path: $(pwd)/key.json"
 }
 
 install
